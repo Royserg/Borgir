8662e69094b8e75521aed315be49d7ca37679eb6
Kuba <staniojakub@gmail.com>
2022-09-25 00:02:39 +0300

    chore(be): cleanup unused project and files

--- backend/Borgir.Domain/ValueObjects/Location.cs
+++
@@ -1,7 +0,0 @@
-namespace Borgir.Domain.Entities;
-
-public record Location(string Country,
-                            string City,
-                            string Street,
-                            string ZipCode);
-
