51ba8f740699ac09bc0ac359c54ef2a83a281fa3
Kuba <staniojakub@gmail.com>
2022-09-25 23:17:20 +0300

    chore(fe): add initial Restaurant details page

---
+++ frontend/src/pages/[id].tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const RestaurantPage: NextPage = () => {
+  return (
+    <>
+      <h2>Restaurant Details</h2>
+    </>
+  );
+};
+
+export default RestaurantPage;
