98707b0a128f1b10123c5b53262b69478953547e
Kuba <staniojakub@gmail.com>
2022-09-25 20:12:40 +0300

    feat(fe): create a search field and read user Location

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: ['light'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+  },
 };
