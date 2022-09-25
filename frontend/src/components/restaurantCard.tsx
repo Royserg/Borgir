adec9c7cf1fdd9f7d5f2ae26475ea2846dc7addb
Kuba <staniojakub@gmail.com>
2022-09-25 22:22:42 +0300

    feat(fe): create RestaurantCard component

---
+++ frontend/src/components/restaurantCard.tsx
@@ -0,0 +1,40 @@
+import { IRestaurant } from '../shared/interfaces/restaurant';
+import Image from 'next/future/image';
+import Link from 'next/link';
+
+type RestaurantCardProps = {
+  data: IRestaurant;
+};
+
+export const RestaurantCard = ({
+  data: { id, name, city, country, imageUrl, street, zipCode },
+}: RestaurantCardProps) => {
+  return (
+    <div className='card lg:card-side bg-base-100 shadow-xl mb-1 border'>
+      <figure>
+        <Image
+          className='object-cover h-64 w-96'
+          loader={() => imageUrl}
+          src={imageUrl}
+          alt='Restaurant photo'
+          width={250}
+          height={200}
+        />
+      </figure>
+      <div className='card-body'>
+        <h2 className='card-title'>{name}</h2>
+
+        <p className='text-sm text-gray-500'>
+          {city}, {country}
+          <p className='text-gray-400'>{street}</p>
+        </p>
+
+        <div className='card-actions justify-end'>
+          <Link href={id}>
+            <button className='btn btn-primary'>View</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
