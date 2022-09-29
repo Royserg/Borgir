cf63789bd91a700b2d2ce5a8080d419b7bf245e5
Kuba <staniojakub@gmail.com>
2022-09-29 22:42:15 +0300

    refactor(fe): store searchTerm, add conditions to result rendering

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,73 +2,93 @@ import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef } from "react";
 import { GiHamburger } from "react-icons/gi";
-import { ImSpinner2 } from "react-icons/im";
 import { RiEmotionSadLine } from "react-icons/ri";
 import { RestaurantCard } from "../components/restaurantCard";
 import { useDebounce } from "../hooks/useDebounce";
 import { getUserCity } from "../services/geolocation";
 import { getRestaurants } from "../services/restaurants";
-import { IGetRestaurants, IRestaurant } from "../shared/interfaces/restaurant";
-import { useRestaurantStore } from "../store/restaurants";
+import { IGetRestaurants } from "../shared/interfaces/restaurant";
+import { useRestaurantStore } from "../store/restaurant";
 
 const Home: NextPage = () => {
-  const searchTerm = useRestaurantStore((state) => state.searchTerm);
-  const setSearchTerm = useRestaurantStore((state) => state.setSearchTerm);
-
-  const [results, setResults] = useState<{ items: IRestaurant[] }>();
-  const { isFetching, refetch } = useQuery<IGetRestaurants, AxiosError>(
-    ["restaurants"],
-    async () => {
-      const data = await getRestaurants(searchTerm);
-      setResults(data);
-      return data;
-    },
-    { enabled: false }
-  );
-
-  /*   const [searchTerm, setSearchTerm] = useState(""); */
-  const debouncedSearchTerm = useDebounce(searchTerm, 500);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  // Effect for API call
+  const { searchTerm, setSearchTerm } = useRestaurantStore((state) => state);
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  const { data, isFetching, isError, error, refetch } = useQuery<
+    IGetRestaurants,
+    AxiosError
+  >(["restaurants"], () => getRestaurants(debouncedSearchTerm), {
+    enabled: !!debouncedSearchTerm,
+  });
+
   useEffect(() => {
     if (debouncedSearchTerm) {
       refetch();
-    } else {
-      setResults(undefined);
     }
   }, [debouncedSearchTerm, refetch]);
 
-  // Effect for getting user city
+  const setSearchInputValue = (value: string) => {
+    // @ts-ignore
+    inputRef.current.value = value;
+  };
+
+  // Effect for getting user city (on initial render only)
   useEffect(() => {
     if (searchTerm) {
-      // @ts-ignore
-      inputRef.current.value = searchTerm;
-    }
+      setSearchInputValue(searchTerm);
+    } else {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          const {
+            coords: { latitude, longitude },
+          } = position;
 
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        const {
-          coords: { latitude, longitude },
-        } = position;
-
-        const city = await getUserCity({ latitude, longitude });
-        if (city) {
-          setSearchTerm(city);
-          if (inputRef) {
-            // @ts-ignore
-            inputRef.current.value = city;
+          const city = await getUserCity({ latitude, longitude });
+          if (city) {
+            setSearchTerm(city);
+            if (inputRef) {
+              setSearchInputValue(city);
+            }
           }
-        }
-      });
+        });
+      }
     }
   }, []);
 
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     const searchValue = e.target.value;
     setSearchTerm(searchValue);
+  };
+
+  const renderSearchResults = () => {
+    if (isFetching) {
+      return <div className="mt-8 text-2xl">Loading...</div>;
+    }
+    if (isError) {
+      return <div className="mt-8 text-2xl">{error.message}</div>;
+    }
+    if (data?.items.length === 0 && searchTerm) {
+      return (
+        <h3 className="text-2xl mt-10 text-center w-1/2">
+          <RiEmotionSadLine className="text-5xl mx-auto" />
+          <br />
+          No restaurants registered in the system for this search term
+        </h3>
+      );
+    }
+    if (data?.items) {
+      return (
+        <ul>
+          {data.items.map((restaurant) => (
+            <RestaurantCard key={restaurant.id} data={restaurant} />
+          ))}
+        </ul>
+      );
+    }
   };
 
   return (
@@ -80,6 +100,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+        {/* Title line */}
         <GiHamburger className="text-7xl" />
         <h1 className="text-5xl md:text-[4rem] leading-normal font-extrabold text-gray-700 text-center mb-4">
           Find your next burger
@@ -99,30 +120,8 @@ const Home: NextPage = () => {
         {/* Divider */}
         <div className="mb-3"></div>
 
-        {/* Loading */}
-        {isFetching ? (
-          <ImSpinner2 className="text-5xl mx-auto" />
-        ) : (
-          <>
-            {results && (
-              <>
-                {results.items?.length === 0 && searchTerm ? (
-                  <h3 className="text-2xl mt-10 text-center w-1/2">
-                    <RiEmotionSadLine className="text-5xl mx-auto" />
-                    <br />
-                    No restaurants registered in the system for this search term
-                  </h3>
-                ) : (
-                  <ul>
-                    {results.items.map((restaurant) => (
-                      <RestaurantCard key={restaurant.id} data={restaurant} />
-                    ))}
-                  </ul>
-                )}
-              </>
-            )}
-          </>
-        )}
+        {/* Restaurant search results */}
+        {renderSearchResults()}
       </main>
     </>
   );
